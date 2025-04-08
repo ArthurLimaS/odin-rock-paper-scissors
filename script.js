005b3ef2e1749899e3cadbf8629731a75e7a8a8e
Arthur Lima <als.arthurlimasilva@gmail.com>
2025-04-08 09:20:54 -0300

    Create function to execute one round of the game

--- script.js
+++ script.js
@@ -1,24 +1,86 @@
 let humanScore = 0
 let computerScore = 0
 
-
 function getComputerChoice () {
     choice = Math.floor(Math.random() * 3)
 
     switch (choice) {
         case 0:
-            return 'R'
+            return 'rock'
         
         case 1:
-            return 'P'
+            return 'paper'
         
         default:
-            return 'S'
+            return 'scissors'
     }
 }
 
 function getHumanChoice() {
     let playerChoice = prompt("Which hand do you wish to play? (Rock, Paper or Scissors)")
 
-    console.log(playerChoice)
+    console.log(playerChoice.toLocaleLowerCase())
+    return playerChoice.toLocaleLowerCase()
+}
+
+function playRound(humanChoice, computerChoice) {
+    let roundResult = ""
+
+    switch (humanChoice) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'rock':
+                    roundResult = "It's a draw! Both chose Rock."
+                    break
+
+                case 'paper':
+                    roundResult = "You lose! Paper beats Rock."
+                    computerScore += 1
+                    break
+
+                case 'scissors':
+                    roundResult = "You win! Rock beats Scissors."
+                    humanScore +=1
+                    break
+            }
+            break
+        
+        case 'paper':
+            switch (computerChoice) {
+                case 'rock':
+                    roundResult = "You win! Paper beats Rock."
+                    humanScore +=1
+                    break
+                    
+                case 'paper':
+                    roundResult = "It's a draw! Both chose Paper."
+                    break
+
+                case 'scissors':
+                    roundResult = "You lose! Scissors beats Paper."
+                    computerScore += 1
+                    break
+            }
+            break
+
+        case 'scissors':
+            switch (computerChoice) {
+                case 'rock':
+                    roundResult = "You lose! Rock beats Scissors."
+                    computerScore += 1
+                    break
+                    
+                case 'paper':
+                    roundResult = "You win! Scissors beats Paper."
+                    humanScore +=1
+                    break
+                        
+                case 'scissors':
+                    roundResult = "It's a draw! Both chose Scissors."
+                    break
+            }
+            break
+    }
+
+    console.log(roundResult)
 }
