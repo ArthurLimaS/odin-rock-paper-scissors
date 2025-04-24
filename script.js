bb63ad35174dab5e59b71ebcd8a69de4aeee0538
Arthur Lima <als.arthurlimasilva@gmail.com>
2025-04-24 09:12:07 -0300

    Add onClick events to the buttons

--- script.js
+++ script.js
@@ -13,95 +13,103 @@ function getComputerChoice () {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    let roundResult = ""
+
+    switch (humanChoice) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'rock':
+                    roundResult = "It's a draw! Both chose Rock."
+                    break
+
+                case 'paper':
+                    roundResult = "You lose! Paper beats Rock."
+                    // computerScore += 1
+                    break
+
+                case 'scissors':
+                    roundResult = "You win! Rock beats Scissors."
+                    // humanScore +=1
+                    break
+            }
+            break
+        
+        case 'paper':
+            switch (computerChoice) {
+                case 'rock':
+                    roundResult = "You win! Paper beats Rock."
+                    // humanScore +=1
+                    break
+                    
+                case 'paper':
+                    roundResult = "It's a draw! Both chose Paper."
+                    break
+
+                case 'scissors':
+                    roundResult = "You lose! Scissors beats Paper."
+                    // computerScore += 1
+                    break
+            }
+            break
+
+        case 'scissors':
+            switch (computerChoice) {
+                case 'rock':
+                    roundResult = "You lose! Rock beats Scissors."
+                    // computerScore += 1
+                    break
+                    
+                case 'paper':
+                    roundResult = "You win! Scissors beats Paper."
+                    // humanScore +=1
+                    break
+                        
+                case 'scissors':
+                    roundResult = "It's a draw! Both chose Scissors."
+                    break
+            }
+            break
+    }
+
+    console.log(roundResult)
+}
+
+const btns = document.querySelectorAll("button")
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        playRound(btn.textContent.toLowerCase(), getComputerChoice())
+    })
+})
+
 function getHumanChoice() {
     let playerChoice = prompt("Which hand do you wish to play? (Rock, Paper or Scissors)")
 
     return playerChoice.toLocaleLowerCase()
 }
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+// function playGame() {
+//     let humanScore = 0
+//     let computerScore = 0
 
-    function playRound(humanChoice, computerChoice) {
-        let roundResult = ""
+//     for (let i = 0; i < 5; i++) {
+//         const humanChoice = getHumanChoice()
+//         const computerChoice = getComputerChoice()
 
-        switch (humanChoice) {
-            case 'rock':
-                switch (computerChoice) {
-                    case 'rock':
-                        roundResult = "It's a draw! Both chose Rock."
-                        break
+//         playRound(humanChoice, computerChoice)
+//     }
 
-                    case 'paper':
-                        roundResult = "You lose! Paper beats Rock."
-                        computerScore += 1
-                        break
+//     console.log("The final game results are:")
+//     console.log("W: " + humanScore + " | D: " + (5 - (computerScore + humanScore)) + " | L: " + computerScore)
 
-                    case 'scissors':
-                        roundResult = "You win! Rock beats Scissors."
-                        humanScore +=1
-                        break
-                }
-                break
-            
-            case 'paper':
-                switch (computerChoice) {
-                    case 'rock':
-                        roundResult = "You win! Paper beats Rock."
-                        humanScore +=1
-                        break
-                        
-                    case 'paper':
-                        roundResult = "It's a draw! Both chose Paper."
-                        break
-
-                    case 'scissors':
-                        roundResult = "You lose! Scissors beats Paper."
-                        computerScore += 1
-                        break
-                }
-                break
-
-            case 'scissors':
-                switch (computerChoice) {
-                    case 'rock':
-                        roundResult = "You lose! Rock beats Scissors."
-                        computerScore += 1
-                        break
-                        
-                    case 'paper':
-                        roundResult = "You win! Scissors beats Paper."
-                        humanScore +=1
-                        break
-                            
-                    case 'scissors':
-                        roundResult = "It's a draw! Both chose Scissors."
-                        break
-                }
-                break
-        }
-
-        console.log(roundResult)
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice()
-        const computerChoice = getComputerChoice()
-
-        playRound(humanChoice, computerChoice)
-    }
-
-    console.log("The final game results are:")
-    console.log("W: " + humanScore + " | D: " + (5 - (computerScore + humanScore)) + " | L: " + computerScore)
-
-    if (humanScore > computerScore) {
-        console.log("You've WON against the machine!")
-    }
-    else if (humanScore < computerScore) {
-        console.log("You've LOST against the machine...")
-    }
-    else {
-        console.log("Its a draw!")
-    }
-}
+//     if (humanScore > computerScore) {
+//         console.log("You've WON against the machine!")
+//     }
+//     else if (humanScore < computerScore) {
+//         console.log("You've LOST against the machine...")
+//     }
+//     else {
+//         console.log("Its a draw!")
+//     }
+// }
